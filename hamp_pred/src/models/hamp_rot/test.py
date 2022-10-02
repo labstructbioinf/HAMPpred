08ca4e4c5b684c3ba9880f3452710d489fa30597
awinski <awinski@cisco.com>
2022-10-02 03:32:03 +0200

    delete debug info

--- hamp_pred/src/models/hamp_rot/test.py
+++ hamp_pred/src/models/hamp_rot/test.py
@@ -4,9 +4,10 @@ from hamp_pred.src.output_analysis.metrics import Metrics
 
 
 class Tester:
-    def __init__(self, out_column='prediction', ignored_vals=None):
+    def __init__(self, out_column='prediction', ignored_vals=None, scale=2):
         self.out_column = out_column
         self.ignored_vals = ignored_vals
+        self.scale = scale
 
     def get_metrics(self, test, prediction):
         mean_rot_true, mean_pred = [], []
@@ -30,7 +31,7 @@ class Tester:
                    'pred_pos_rot': pos_rot_pred,
                    'true_mean_rot': mean_rot_true,
                    'pred_mean_rot': mean_pred}
-        return metrics
+        return self._to_arrays(metrics)
 
     def get_squences(self, data):
         sequences, mean_rot = [], []
@@ -38,4 +39,10 @@ class Tester:
             seq = row['n_seq'][1:-1] + row['c_seq'][1:-1]
             sequences.append(seq)
         return sequences
+
+    def _to_arrays(self, metrics):
+        for key, value in metrics.items():
+            if isinstance(value, list) or isinstance(value, tuple):
+                metrics[key] = np.array(value) / self.scale
+
 
